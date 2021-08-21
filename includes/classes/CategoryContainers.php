6e00af7de92f92fa8f83155a32176bc40c4b12fe
secaidastudio <oscarpaiz79@gmail.com>
2021-08-20 20:15:38 -0600

    Creating TV shows, movies and search features

--- includes/classes/CategoryContainers.php
+++ includes/classes/CategoryContainers.php
@@ -17,6 +17,29 @@ class CategoryContainers{
         return $html . "</div>";
     }
 
+
+    public function showTVShowCategories(){
+        $query = $this->con->prepare("SELECT * FROM categories");
+        $query->execute();
+        $html = "<div class='previewCategories'>
+                    <h1>TV Shows</h1>";
+        while($row=$query->fetch(PDO::FETCH_ASSOC)){
+            $html .= $this->getCategoryHtml($row,null,true,false);
+        }
+        return $html . "</div>";
+    }
+    public function showMovieCategories(){
+        $query = $this->con->prepare("SELECT * FROM categories");
+        $query->execute();
+        $html = "<div class='previewCategories'>
+                    <h1>Movies</h1>";
+        while($row=$query->fetch(PDO::FETCH_ASSOC)){
+            $html .= $this->getCategoryHtml($row,null,false,true);
+        }
+        return $html . "</div>";
+    }
+
+
     public function showCategory($categoryId, $title = null){
         $query = $this->con->prepare("SELECT * FROM categories WHERE id=:id");
         $query->bindValue(":id", $categoryId);
@@ -35,9 +58,9 @@ class CategoryContainers{
     if ($tvShows && $movies) {
         $entities = EntityProvider::getEntities($this->con, $cateogryId, 30);
     }else if($tvShows){
-        //Get tv shows entities
+        $entities = EntityProvider::getTVShowEntities($this->con, $cateogryId, 30);
     }else{
-        //Get movies entities
+        $entities = EntityProvider::getMoviesEntities($this->con, $cateogryId, 30);
     }
 
     if (sizeof($entities) == 0) {
